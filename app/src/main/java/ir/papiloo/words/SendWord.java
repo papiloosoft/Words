d3d97d61e0af755fb88f7eb2d6b0dba9547a1235
amirrasooli69 <amirrasooli69@gmail.com>
2020-05-06 18:56:47 +0430

    add class send word

--- app/src/main/java/ir/papiloo/words/SendWord.java
+++ app/src/main/java/ir/papiloo/words/SendWord.java
@@ -1,87 +1,91 @@
 package ir.papiloo.words;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.os.Bundle;
-import android.view.View;
-import android.webkit.WebView;
-import android.widget.Button;
-import android.widget.Spinner;
-import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.Locale;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class SendWord extends AppCompatActivity {
+
     public static final String URI_SHOW_PARAMS = "https://Papiloo.ir/Papiloo/Game/Insert.php?Language=سنگسری&Word=چاور&Mean=چادر&Pronounce=چاوَر";
 
-    Spinner category;
-    TextView Word,MeanWord, pronounce;
-    Button btnSend;
-
-    private String EncodingUtils;
-    private WebView webView;
-
+    TextView tv,showUrl;
+    ProgressBar pb;
+    List<AsyncTask> tasks = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.send_word);
-        getSupportActionBar().hide();
+        tv = findViewById(R.id.tv);
+        //showUrl=findViewById(R.id.showUrl);
+        pb = findViewById(R.id.progressBar);
+        pb.setVisibility(View.INVISIBLE);
+    }
 
-        category= findViewById(R.id.selectWord);
-        Word=(TextView)findViewById(R.id.txtWord);
-        MeanWord= (TextView)findViewById(R.id.txtMeansWord);
-        pronounce =findViewById(R.id.pronounce);
-        btnSend= (Button) findViewById(R.id.btnSend);
-
-        btnSend.setOnClickListener(new View.OnClickListener()
-        {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuItem item1=menu.add("GET");
+        item1.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        item1.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
-            public void onClick(View v)
-            {
-                String word, mean,category;
-                word= Word.getText().toString();
-                mean=MeanWord.getText().toString();
-                category=
-                try
-                {
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                MyHttpUtils.RequestData requestData =
+                        new MyHttpUtils.RequestData(URI_SHOW_PARAMS,"GET");
+                requestData.setParameter("name","AmirRasooli");
+                requestData.setParameter("message","HelloAmir");
 
-                    MyHttpUtils.RequestData requestData =
-                            new MyHttpUtils.RequestData(URI_SHOW_PARAMS,"GET");
-                    //requestData.setParameter("name","AmirRasooli");
-                    //requestData.setParameter("message","HelloAmir");
 
-                    //new HttpParamsActivity.MyTask().execute(requestData);
+                //showUrl.setText(requestData.toString());
+                new MyTask().execute(requestData);
 
-                }
-                catch (Exception e)
-                {
-                    AlertDialog.Builder alertDialog = new AlertDialog.Builder(SendWord.this);
-                    alertDialog.setTitle("نظر");
-                    alertDialog.setMessage("بزودی فعال میشود");
-                    alertDialog.setIcon(R.drawable.logo);
-                    alertDialog.setPositiveButton("خروج ",
-                            new DialogInterface.OnClickListener()
-                            {
-                                public void onClick(DialogInterface dialog, int which) {
-                                    finish();
-                                }
-                            });
-//                             alertDialog.setNegativeButton("درباره ما",
-//                            new DialogInterface.OnClickListener()
-//                            {
-//                                public void onClick(DialogInterface dialog, int which) {
-//                                    startActivity(new Intent(Home.this, About.class));
-//                                }
-//                            });
-                    alertDialog.show();
-                }
 
+
+                return false;
             }
         });
-    };
+        return super.onCreateOptionsMenu(menu);
+    }
+    public class  MyTask extends AsyncTask<MyHttpUtils.RequestData,Void ,String>
+    {
 
+        @Override
+        protected void onPreExecute()
+        {
+            if(tasks.isEmpty())
+            {
+                pb.setVisibility(View.VISIBLE);
+            }
+            tasks.add(this);
+        }
 
+        @Override
+        protected String doInBackground(MyHttpUtils.RequestData... params) {
+            MyHttpUtils.RequestData reqData = params[0];
+            return  MyHttpUtils.getDataHttpUrlConnection(reqData);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            if (result == null)
+            {
+                result = "null";
+
+            }
+            tv.setText(result);
+            tasks.remove(this);
+            if(tasks.isEmpty())
+            {
+                pb.setVisibility(View.INVISIBLE);
+            }
+        }
+    }
 }
